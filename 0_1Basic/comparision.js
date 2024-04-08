b7624559688e09024d40b0c1dc1c38f98c5ea937
Mingtindu <mingmartu@gmail.com>
2024-04-08 07:22:31 +0545

    comparision

---
+++ 0_1Basic/comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>2);
+
+
+console.log("2">1);
+console.log("02">1);
+
+//we should compare only when the var has same datatype 
+
+console.log (null>0);//false
+console.log (null==0);//false
+console.log (null>=0);//true
+//this type of comarision we should avoid:::
+
+console.log(undefined==0);
+
+
+//=== is defferent fron == 
+//=== check the datatype before comparing
+//
