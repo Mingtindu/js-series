2a9b2c37840addfad36c52fe8c57e20f7e968e9f
Mingtindu <mingmartu@gmail.com>
2024-04-08 07:41:33 +0545

    datatype final

---
+++ 0_1Basic/datatypeFinal.js
@@ -0,0 +1,31 @@
+// Primitive datatype:
+
+//7
+//:String Number Boolean Null undefined Symbol BigInt
+
+//dynamically typed language no need to declare datatype;
+
+
+
+
+const id = Symbol('mingtindu');
+const anotherId= Symbol('mingtindu');
+console.log(id ==anotherId);
+//Symbol datatype is used to declare unique value:
+
+//ReferenceError(Non primitive)
+
+//Array , onjects ,functions
+const heros = ["spiderman","batman","ironman"];
+
+let myobg = {
+    name:"ming ",
+    age:10,
+
+
+
+
+}
+const myFunction =function(){
+console.log("hello world");
+}
