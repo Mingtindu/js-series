d604ac1aaa7ecac6fdd93c70dc856fbd0906698c
Mingtindu <mingmartu@gmail.com>
2024-04-16 16:54:25 +0545

    feat:String

---
+++ 0_1Basic/string.js
@@ -0,0 +1,31 @@
+const name = "Mingtindu";
+
+
+//String is object in js 
+const newName = new String('mingtindu-sh');
+console.log(newName.__proto__);// method to access string
+
+
+//methods:
+console.log(newName.toLocaleLowerCase);
+// console.log(newName.charAt(1));
+// console.log(newName.indexOf('t'));
+
+const newString =newName.substring(0,4);
+console.log(newString);
+
+const anotherS= game.slice(-4,3);
+
+const str1 = "    Mingtindu   Sherpa";
+console.log(str1.trim());//remove spaces in string:
+
+const url = "https://localhost/st/ming%20sherpa"
+
+url.replace('%20','-');//replace the string ::
+
+url.includes('ming');//check if the string is include or not 
+
+console.log(newName.split(s));
+
+
+
