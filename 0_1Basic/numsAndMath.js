04cdff2da4a3a883be382d9fc999f9f37b6aae95
Mingtindu <mingmartu@gmail.com>
2024-04-17 07:54:45 +0545

    feat: Math and Number in js

---
+++ 0_1Basic/numsAndMath.js
@@ -0,0 +1,34 @@
+const score = 400;
+
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));//number after decimal::
+
+// const num = 23.2568;
+
+// console.log(num.toPrecision(3));//round off:
+
+// const hundred = 100000
+
+// console.log(hundred.toLocaleString());
+
+
+
+///// ++++++++++++++++MATH+++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-10));//conver to absolute neg to pos:
+// console.log(Math.round(4.6));//round off::
+
+console.log((Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min +1))+min);
+
