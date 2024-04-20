b8ecc892c98273ed2f621e1f9ac7655269908106
Mingtindu <mingmartu@gmail.com>
2024-04-20 18:15:29 +0545

    feat: Array in js

---
+++ 0_2Basic/array.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4];
+//Datatype can be mixed in an array:
+//Can be change size
+
+const people = ["Starck","Williamson"]
+const numberArr = new Array(1,2,5,4,5);
+
+//Methods in an array:
+
+numberArr.push(10);
+numberArr.push(20);
+numberArr.shift();//remove the value from the beginning of the array
+numberArr.unshift(5);//add the value at the beginning of the array:
+
+
+console.log(numberArr);
